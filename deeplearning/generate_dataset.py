ef967632a3bbb1cb7348d44cbb07cfbeffb2c483
DECHELLE-RIALLAND Remy <remy.dechelle-rialland@etu.univ-amu.fr>
2020-01-26 00:26:42 +0100

    Création dataset + tests GAN

--- deeplearning/generate_dataset.py
+++ deeplearning/generate_dataset.py
@@ -1,77 +1,39 @@
-import cv2
-import glob
 import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
 
-# Train data
-train = []
-train_labels = []
+DATADIR = "data/training"
+CATEGORIES = ["terrains", "not_terrains"]
 
-files = glob.glob("data/training/terrains/*.tif")  # your image path
-for trainFile in files:
-    image = cv2.imread(trainFile)
-    train.append(image)
-    train_labels.append([1., 0.])
+IMG_SIZE = 600
+training_data = []
 
-files = glob.glob("data/training/not_terrains/*.jpg")
-for trainFile in files:
-    image = cv2.imread(trainFile)
-    train.append(image)
-    train_labels.append([0., 1.])
 
-train = np.array(train, dtype='float32')  # as mnist
-train_labels = np.array(train_labels, dtype='float64')  # as mnist
-# convert (number of images x height x width x number of channels) to (number of images x (height * width *3))
-# for example (120 * 40 * 40 * 3)-> (120 * 4800)
-train = np.reshape(train, [train.shape[0], train.shape[1] * train.shape[2] * train.shape[3]])
+def create_training_data():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR, category)
+        class_num = CATEGORIES.index(category)
+        for img in os.listdir(path):
+            if not img.endswith(('.tif', '.jpg')):
+                continue
+            print(img)
+            try:
+                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                training_data.append([new_array, class_num])
+            except Exception as e:
+                pass
 
-# save numpy array as .npy formats
-np.save('datasets/train', train)
-np.save('datasets/train_labels', train_labels)
 
-# Test data
-validation = []
-validation_labels = []
-files = glob.glob("data/validation/terrains/*.tif")
-for myFile in files:
-    image = cv2.imread(myFile)
-    validation.append(image)
-    validation_labels.append([1., 0.])  # class1
-files = glob.glob("data/validation/not_terrains/*.jpg")
-for myFile in files:
-    image = cv2.imread(myFile)
-    validation.append(image)
-    validation_labels.append([0., 1.])  # class2
+create_training_data()
+print(len(training_data))
 
-validation = np.array(validation, dtype='float32')  # as mnist example
-validation_labels = np.array(validation_labels, dtype='float64')  # as mnist
-validation = np.reshape(validation, [validation.shape[0], validation.shape[1] * validation.shape[2] * validation.shape[3]])
+X = []
+y = []
 
-# save numpy array as .npy formats
-np.save('datasets/validation', validation)  # saves test.npy
-np.save('datasets/validation_labels', validation_labels)
+for features, label in training_data:
+    X.append(features)
+    y.append(label)
 
-# Importing required libraries
-# import os
-# import numpy as np
-# from PIL import Image
-#
-# IMAGE_SIZE = 600
-# IMAGE_CHANNELS = 3
-# IMAGE_DIR = "data/gan"
-#
-# # Defining image dir path. Change this if you have different directory
-# images_path = IMAGE_DIR
-#
-# training_data = []
-#
-# for file in os.listdir(images_path):
-#     if file.endswith((".tif", ".jpg")):
-#         path = os.path.join(images_path, file)
-#         image = Image.open(path).resize((IMAGE_SIZE, IMAGE_SIZE), Image.ANTIALIAS)
-#         training_data.append(np.asarray(image))
-#
-# training_data = np.reshape(training_data, (-1, IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS))
-# training_data = training_data / 127.5 - 1
-#
-# print('Saving...')
-# np.save('datasets/dcgan.npy', training_data)
+np.save('datasets/dcgan.npy', X)
