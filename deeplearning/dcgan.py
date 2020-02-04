95c912173fb64f4a99ca10a0b7c56ef7ae6151a2
DECHELLE-RIALLAND Remy <remy.dechelle-rialland@etu.univ-amu.fr>
2020-02-04 14:35:04 +0100

    DCGAN mieux

--- deeplearning/dcgan.py
+++ deeplearning/dcgan.py
@@ -1,177 +1,283 @@
-from __future__ import print_function, division
+from keras.models import Sequential, Model, load_model
+from keras.layers import UpSampling2D, Conv2D, Activation, BatchNormalization, Reshape, Dense, Input, LeakyReLU, Dropout, Flatten, ZeroPadding2D
+from keras.optimizers import Adam
 
-from keras.datasets import mnist
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout
-from keras.layers import BatchNormalization, Activation, ZeroPadding2D
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import UpSampling2D, Conv2D
-from keras.models import Sequential, Model
-from tensorflow.keras.optimizers import Adam
-
-import tensorflow as tf
-import matplotlib.pyplot as plt
-
-import sys
-
+import glob
+from PIL import Image
 import numpy as np
+import os
+import argparse
+from ast import literal_eval
+from imageio import imsave
 
 
-class DCGAN():
-    def __init__(self):
-        # Input shape
-        self.img_rows = 28
-        self.img_cols = 28
-        self.channels = 1
-        self.img_shape = (self.img_rows, self.img_cols, self.channels)
-        self.latent_dim = 100
-
-        optimizer = Adam(0.0002, 0.5)
-
-        # Build and compile the discriminator
-        self.discriminator = self.build_discriminator()
-        self.discriminator.compile(loss='binary_crossentropy',
-                                   optimizer=optimizer,
-                                   metrics=['accuracy'])
-
-        # Build the generator
-        self.generator = self.build_generator()
-
-        # The generator takes noise as input and generates imgs
-        z = Input(shape=(self.latent_dim,))
-        img = self.generator(z)
-
-        # For the combined model we will only train the generator
-        self.discriminator.trainable = False
-
-        # The discriminator takes generated images as input and determines validity
-        valid = self.discriminator(img)
-
-        # The combined model  (stacked generator and discriminator)
-        # Trains the generator to fool the discriminator
-        self.combined = Model(z, valid)
-        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+class DCGAN:
+    def __init__(self, discriminator_path, generator_path, output_directory, img_size):
+        self.img_size = img_size
+        self.upsample_layers = 5
+        self.starting_filters = 64
+        self.kernel_size = 3
+        self.channels = 3
+        self.discriminator_path = discriminator_path
+        self.generator_path = generator_path
+        self.output_directory = output_directory
 
     def build_generator(self):
+        noise_shape = (100,)
 
+        # This block of code can be a little daunting, but essentially it automatically calculates the required starting
+        # array size that will be correctly upscaled to our desired image size.
+        #
+        # We have 5 Upsample2D layers which each double the images width and height, so we can determine the starting
+        # x size by taking (x / 2^upsample_count) So for our target image size, 256x192, we do the following:
+        # x = (192 / 2^5), y = (256 / 2^5) [x and y are reversed within the model]
+        # We also need a 3rd dimension which is chosen relatively arbitrarily, in this case it's 64.
         model = Sequential()
+        model.add(
+            Dense(self.starting_filters * (self.img_size[0] // (2 ** self.upsample_layers))  *  (self.img_size[1] // (2 ** self.upsample_layers)),
+                  activation="relu", input_shape=noise_shape))
+        model.add(Reshape(((self.img_size[0] // (2 ** self.upsample_layers)),
+                           (self.img_size[1] // (2 ** self.upsample_layers)),
+                           self.starting_filters)))
+        model.add(BatchNormalization(momentum=0.8))
 
-        model.add(Dense(128 * 7 * 7, activation="relu", input_dim=self.latent_dim))
-        model.add(Reshape((7, 7, 128)))
-        model.add(UpSampling2D())
-        model.add(Conv2D(128, kernel_size=3, padding="same"))
-        model.add(BatchNormalization(momentum=0.8))
+        model.add(UpSampling2D())  # 6x8 -> 12x16
+        model.add(Conv2D(1024, kernel_size=self.kernel_size, padding="same"))
         model.add(Activation("relu"))
-        model.add(UpSampling2D())
-        model.add(Conv2D(64, kernel_size=3, padding="same"))
         model.add(BatchNormalization(momentum=0.8))
+
+        model.add(UpSampling2D())  # 12x16 -> 24x32
+        model.add(Conv2D(512, kernel_size=self.kernel_size, padding="same"))
         model.add(Activation("relu"))
-        model.add(Conv2D(self.channels, kernel_size=3, padding="same"))
+        model.add(BatchNormalization(momentum=0.8))
+
+        model.add(UpSampling2D())  # 24x32 -> 48x64
+        model.add(Conv2D(256, kernel_size=self.kernel_size, padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+
+        model.add(UpSampling2D())  # 48x64 -> 96x128
+        model.add(Conv2D(128, kernel_size=self.kernel_size, padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+
+        model.add(UpSampling2D())  # 96x128 -> 192x256
+        model.add(Conv2D(64, kernel_size=self.kernel_size, padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+
+        model.add(Conv2D(32, kernel_size=self.kernel_size, padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+
+        model.add(Conv2D(self.channels, kernel_size=self.kernel_size, padding="same"))
         model.add(Activation("tanh"))
 
         model.summary()
 
-        noise = Input(shape=(self.latent_dim,))
+        noise = Input(shape=noise_shape)
         img = model(noise)
 
         return Model(noise, img)
 
     def build_discriminator(self):
 
+        img_shape = (self.img_size[0], self.img_size[1], self.channels)
+
         model = Sequential()
 
-        model.add(Conv2D(32, kernel_size=3, strides=2, input_shape=self.img_shape, padding="same"))
+        model.add(Conv2D(32, kernel_size=self.kernel_size, strides=2, input_shape=img_shape, padding="same"))  # 192x256 -> 96x128
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.25))
-        model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
+
+        model.add(Conv2D(64, kernel_size=self.kernel_size, strides=2, padding="same"))  # 96x128 -> 48x64
         model.add(ZeroPadding2D(padding=((0, 1), (0, 1))))
-        model.add(BatchNormalization(momentum=0.8))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.25))
-        model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
         model.add(BatchNormalization(momentum=0.8))
+
+        model.add(Conv2D(128, kernel_size=self.kernel_size, strides=2, padding="same"))  # 48x64 -> 24x32
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.25))
-        model.add(Conv2D(256, kernel_size=3, strides=1, padding="same"))
         model.add(BatchNormalization(momentum=0.8))
+
+        model.add(Conv2D(256, kernel_size=self.kernel_size, strides=1, padding="same"))  # 24x32 -> 12x16
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.25))
+
+        model.add(Conv2D(512, kernel_size=self.kernel_size, strides=1, padding="same"))  # 12x16 -> 6x8
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+
         model.add(Flatten())
         model.add(Dense(1, activation='sigmoid'))
 
         model.summary()
 
-        img = Input(shape=self.img_shape)
+        img = Input(shape=img_shape)
         validity = model(img)
 
         return Model(img, validity)
 
-    def train(self, epochs, batch_size=128, save_interval=50):
+    def build_gan(self):
+        optimizer = Adam(0.0002, 0.5)
 
-        # Load the dataset
-        X_train = np.load("datasets/dcgan.npy")
-        print(X_train.shape)
-        # X_train = tf.reshape(X_train, [-1, 28, 28, 1])
+        # See if the specified model paths exist, if they don't then we start training new models
 
-        # Rescale -1 to 1
-        X_train = X_train / 127.5 - 1.
-        X_train = np.expand_dims(X_train, axis=3)
+        if os.path.exists(self.discriminator_path) and os.path.exists(self.generator_path):
+            self.discriminator = load_model(self.discriminator_path)
+            self.generator = load_model(self.generator_path)
+            print("Loaded models...")
+        else:
+            self.discriminator = self.build_discriminator()
+            self.discriminator.compile(loss='binary_crossentropy',
+                                       optimizer=optimizer,
+                                       metrics=['accuracy'])
 
-        # Adversarial ground truths
-        valid = np.ones((batch_size, 1))
-        fake = np.zeros((batch_size, 1))
+            self.generator = self.build_generator()
+            self.generator.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+        # These next few lines setup the training for the GAN model
+        z = Input(shape=(100,))
+        img = self.generator(z)
+
+        self.discriminator.trainable = False
+
+        valid = self.discriminator(img)
+
+        self.combined = Model(z, valid)
+        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+    def load_imgs(self, image_path):
+        X_train = []
+        for i in glob.glob(image_path):
+            img = Image.open(i)
+            img = np.asarray(img)
+            X_train.append(img)
+        return np.asarray(X_train)
+
+    def train(self, epochs, image_path, batch_size=32, save_interval=50):
+        self.build_gan()
+        X_train = self.load_imgs(image_path)
+        print("Training Data Shape: ", X_train.shape)
+
+        # Rescale images from -1 to 1
+        X_train = (X_train.astype(np.float32) - 127.5) / 127.5
+
+        half_batch = batch_size // 2
 
         for epoch in range(epochs):
 
-            # ---------------------
-            #  Train Discriminator
-            # ---------------------
 
-            # Select a random half of images
-            idx = np.random.randint(0, X_train.shape[0], batch_size)
+            # Train Generator
+            noise = np.random.normal(0, 1, (batch_size, 100))
+            g_loss = self.combined.train_on_batch(noise, np.ones((batch_size, 1)))
+
+
+
+            # Train Discriminator
+            idx = np.random.randint(0, X_train.shape[0], half_batch)
             imgs = X_train[idx]
 
-            # Sample noise and generate a batch of new images
-            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+            # Sample noise and generate a half batch of new images
+            noise = np.random.normal(0, 1, (half_batch, 100))
             gen_imgs = self.generator.predict(noise)
 
             # Train the discriminator (real classified as ones and generated as zeros)
-            d_loss_real = self.discriminator.train_on_batch(imgs, valid)
-            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
+            d_loss_real = self.discriminator.train_on_batch(imgs, np.ones((half_batch, 1)))
+            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, np.zeros((half_batch, 1)))
             d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
-            # ---------------------
-            #  Train Generator
-            # ---------------------
+            # Print progress
+            print(f"{epoch} [D loss: {d_loss[0]} | D Accuracy: {100 * d_loss[1]}] [G loss: {g_loss}]")
 
-            # Train the generator (wants discriminator to mistake images as real)
-            g_loss = self.combined.train_on_batch(noise, valid)
+            # If at save interval => save generated image samples, save model files
+            if epoch % (save_interval) == 0:
 
-            # Plot the progress
-            print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100 * d_loss[1], g_loss))
-
-            # If at save interval => save generated image samples
-            if epoch % save_interval == 0:
                 self.save_imgs(epoch)
+
+                save_path = self.output_directory + "/models"
+                if not os.path.exists(save_path):
+                    os.makedirs(save_path)
+                self.discriminator.save(save_path + "/discrim.h5")
+                self.generator.save(save_path + "/generat.h5")
+
+    def gene_imgs(self, count):
+        # Generate images from the currently loaded model
+        noise = np.random.normal(0, 1, (count, 100))
+        return self.generator.predict(noise)
 
     def save_imgs(self, epoch):
         r, c = 5, 5
-        noise = np.random.normal(0, 1, (r * c, self.latent_dim))
-        gen_imgs = self.generator.predict(noise)
 
-        # Rescale images 0 - 1
-        gen_imgs = 0.5 * gen_imgs + 0.5
+        # Generates r*c images from the model, saves them individually and as a gallery
 
-        fig, axs = plt.subplots(r, c)
-        cnt = 0
-        for i in range(r):
-            for j in range(c):
-                axs[i, j].imshow(gen_imgs[cnt, :, :, 0], cmap='gray')
-                axs[i, j].axis('off')
-                cnt += 1
-        fig.savefig("data/generated/mnist_%d.png" % epoch)
-        plt.close()
+        imgs = self.gene_imgs(r*c)
+        imgs = 0.5 * imgs + 0.5
+
+        for i, img_array in enumerate(imgs):
+            path = f"{self.output_directory}/generated_{self.img_size[0]}x{self.img_size[1]}"
+            if not os.path.exists(path):
+                os.makedirs(path)
+            imsave(path + f"/{epoch}_{i}.png", img_array)
+
+        nindex, height, width, intensity = imgs.shape
+        nrows = nindex // c
+        assert nindex == nrows * c
+        # want result.shape = (height*nrows, width*ncols, intensity)
+        gallery = (imgs.reshape(nrows, c, height, width, intensity)
+                  .swapaxes(1, 2)
+                  .reshape(height * nrows, width * c, intensity))
+
+        path = f"{self.output_directory}/gallery_generated_{self.img_size[0]}x{self.img_size[1]}"
+        if not os.path.exists(path):
+            os.makedirs(path)
+        imsave(path + f"/{epoch}.png", gallery)
+
+    def generate_imgs(self, count, threshold, modifier):
+        self.build_gan()
+
+        # Generates (count) images from the model ensuring the discriminator scores them between the threshold values
+        # and saves them
+
+        imgs = []
+        for i in range(count):
+            score = [0]
+            while not(threshold[0] < score[0] < threshold[1]):
+                img = self.gene_imgs(1)
+                score = self.discriminator.predict(img)
+            print("Image found: ", score[0])
+            imgs.append(img)
+
+        imgs = np.asarray(imgs).squeeze()
+        imgs = 0.5 * imgs + 0.5
+
+        print(imgs.shape)
+        for i, img_array in enumerate(imgs):
+            path = f"{self.output_directory}/generated_{threshold[0]}_{threshold[1]}"
+            if not os.path.exists(path):
+                os.makedirs(path)
+            imsave(path + f"/{modifier}_{i}.png", img_array)
 
 
 if __name__ == '__main__':
-    dcgan = DCGAN()
-    dcgan.train(epochs=4000, batch_size=32, save_interval=50)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--load_generator', help='Path to existing generator weights file', default="../data/models/generat.h5")
+    parser.add_argument('--load_discriminator', help='Path to existing discriminator weights file', default="../data/models/discrim.h5")
+    parser.add_argument('--data', help='Path to directory of images of correct dimensions, using *.[filetype] (e.g. *.png) to reference images', default="data/gan/*.jpg")
+    parser.add_argument('--sample', help='If given, will generate that many samples from existing model instead of training', default=-1)
+    parser.add_argument('--sample_thresholds', help='The values between which a generated image must score from the discriminator', default="(0.0, 0.1)")
+    parser.add_argument('--batch_size', help='Number of images to train on at once', default=24)
+    parser.add_argument('--image_size', help='Size of images as tuple (height,width). Height and width must both be divisible by (2^5)', default="(600, 600)")
+    parser.add_argument('--epochs', help='Number of epochs to train for', default=500000)
+    parser.add_argument('--save_interval', help='How many epochs to go between saves/outputs', default=100)
+    parser.add_argument('--output_directory', help="Directoy to save weights and images to.", default="data/output/")
+
+    args = parser.parse_args()
+
+    dcgan = DCGAN(args.load_discriminator, args.load_generator, args.output_directory, literal_eval(args.image_size))
+    if args.sample == -1:
+        dcgan.train(epochs=int(args.epochs), image_path=args.data, batch_size=int(args.batch_size), save_interval=int(args.save_interval))
+    else:
+        dcgan.generate_imgs(int(args.sample), literal_eval(args.sample_thresholds), "")
--- deeplearning/generate_dataset.py
+++ deeplearning/generate_dataset.py
@@ -3,37 +3,30 @@ import matplotlib.pyplot as plt
 import os
 import cv2
 
-DATADIR = "data/training"
-CATEGORIES = ["terrains", "not_terrains"]
+DATADIR = "data/gan"
+# CATEGORIES = ["terrains", "not_terrains"]
 
-IMG_SIZE = 600
+IMG_SIZE = 28
 training_data = []
 
 
 def create_training_data():
-    for category in CATEGORIES:
-        path = os.path.join(DATADIR, category)
-        class_num = CATEGORIES.index(category)
-        for img in os.listdir(path):
-            if not img.endswith(('.tif', '.jpg')):
-                continue
-            print(img)
-            try:
-                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
-                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-                training_data.append([new_array, class_num])
-            except Exception as e:
-                pass
+    # for category in CATEGORIES:
+    #     path = os.path.join(DATADIR, category)
+    #     class_num = CATEGORIES.index(category)
+    for img in os.listdir(DATADIR):
+        if not img.endswith(('.tif', '.jpg')):
+            continue
+        print(img)
+        try:
+            img_array = cv2.imread(os.path.join(DATADIR, img), cv2.IMREAD_GRAYSCALE)
+            new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+            training_data.append([new_array])
+        except Exception as e:
+            pass
 
 
 create_training_data()
-print(len(training_data))
 
-X = []
-y = []
-
-for features, label in training_data:
-    X.append(features)
-    y.append(label)
-
-np.save('datasets/dcgan.npy', X)
+training_data = np.array(training_data).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+np.save('datasets/dcgan.npy', training_data)
