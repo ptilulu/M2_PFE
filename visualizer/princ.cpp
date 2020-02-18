b765102104b667431b1352f5c5ddacb9743f565a
Kr4unos <kr4unos@gmail.com>
2020-02-18 18:41:16 +0000

    Mise à l'échelle

--- visualizer/princ.cpp
+++ visualizer/princ.cpp
@@ -19,6 +19,32 @@ void Princ::on_actionOuvrir_triggered()
     this->glarea->setDem(dem);
 
     QFileInfo fileInfo(fileName);
+
+    // Temp génération auto de obj
+    QFile objFile(fileInfo.baseName() + ".obj");
+    if(objFile.open(QIODevice::ReadWrite)){
+        QTextStream stream(&objFile);
+
+        //export in obj
+        uint height = dem->getHeight(), width =dem->getWidth();
+        float x=-(width/2.0f) + 0.5f,z=-(height/2.0f) + 0.5f;
+        for(uint l=0;l<height;l++){
+            for(uint w=0;w<width;w++){
+                stream << "v " << x << " " << dem->elevation_map[l*width+w] << " " << z << endl ;
+                x++;
+            }
+            x=-(width/2.0f) + 0.5f;
+            z++;
+        }
+        uint id=1;
+        for(uint l=0;l<height-1;l++){
+            for(uint w=0;w<width-1;w++){
+                stream << "f " << id << " " << id+1 << " " << id+width+1 << " " << id+width << endl ;
+                id++;
+            }
+            id++;
+        }
+    }
     Ui_Princ::statusBar->showMessage("Fichier " + fileInfo.baseName() + " ouvert avec succès!");
 }
 
